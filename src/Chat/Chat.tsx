4902c77d58073ffa81770dbd140cc9655f44edfc
akulaok <polovina62003@gmail.com>
2025-04-04 18:45:21 +0500

    chat input

--- src/Chat/Chat.tsx
+++ src/Chat/Chat.tsx
@@ -1,6 +1,7 @@
 import {JSX} from "react";
 import styles from "./Chat.module.css";
 import ChatHeader from "../ChatHeader/ChatHeader";
+import ChatInput from "../ChatInput/ChatInput";
 
 interface ChatProps {
   username: string;
@@ -11,6 +12,9 @@ function Chat({username, room}: ChatProps): JSX.Element {
   return (
     <main className={styles.main}>
       <ChatHeader username={username} room={room} />
+      <div className={styles.ChatContainer}>
+        <ChatInput />
+      </div>
     </main>
   );
 }
